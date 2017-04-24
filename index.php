011bd627b941e7521cd765d6bdd0951f6de1ea32
D3v3l0per1 <manuel.widmoser12@gmail.com>
2017-04-24 04:27:06 +0200

    Added Files
    Ground files and a lil bit of HTML and CSS

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Social Network</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/include.css">
+  </head>
+  <body>
+    <div class="logop">
+      <img src="img/" alt="logo">
+    </div>
+    <div class="sub-nav">
+
+    </div>
+    <div class="content">
+      <div class="nav">
+        <div class="main-content">
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
